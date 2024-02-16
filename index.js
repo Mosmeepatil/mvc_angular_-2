8f11ba33b53263f3fe8d0aa43a9242ec89a6f0cd
siddhi <siddhima@cybage.com>
2024-02-16 10:42:19 +0530

    added command in index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors({
     credentials:true,
     origin:["http://localhost:4200"]
 }))
-
+//comment for git hub by siddhi
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use("/", employeeRouter);
